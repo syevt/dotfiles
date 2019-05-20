135c310ac76dc58eb4b439abe8413dd949bbe6bd
Serhiy Yevtushenko <serhiy.yevtushenko@gmail.com>
2019-05-20 10:56:46 +0300

    Add oh-my-zsh autocomplete plugin

---
+++ config.zsh
@@ -0,0 +1,5 @@
+# put this to $ZSH_CUSTOM/<any-name>.zsh
+# and set in .zshrc plugins(... zsh-autosuggestions)
+ZSH_AUTOSUGGEST_STRATEGY=(match_prev_cmd)
+ZSH_AUTOSUGGEST_BUFFER_MAX_SIZE=40
+bindkey '^ ' autosuggest-accept
