90a1775bf60b477b7b501d06e3ffbe847920b7e3
Serhiy Yevtushenko <syevtush@cisco.com>
2019-10-04 17:53:45 +0300

    Add battery icons to tmux battery.sh

--- .tmux/segments/battery.sh
+++ .tmux/segments/battery.sh
@@ -4,46 +4,52 @@ runSegment() {
   # get battery level
 
   # this was for MacOS
-  # battery=$(pmset -g batt | egrep "([0-9]+\%).*" -o | cut -f1 -d';')
+  battery=$(pmset -g batt | egrep "([0-9]+\%).*" -o | cut -f1 -d';')
+  level=${battery%?} # removing last char '%'
 
   # check if on ac or battery power
-
   # this is for MacOS
-  # if [[ $(pmset -g ps | head -1) =~ "AC Power" ]]; then
+  charging=""
+  if [[ $(pmset -g ps | head -1) =~ "AC Power" ]]
+  then
+    charging=""
+  fi
   # level=$(acpi | cut -d ' ' -f 4)
+  # battery=$(pmset -g batt | egrep "([0-9]+\%).*" -o | cut -f1 -d';')
 
-  # icon=""
+  icon=""
   # percentage=$(acpi | cut -d ' ' -f 4):0:2
-  # level=$((($percentage:0:2) + 0))
+  # level=$((($battery:0:2) + 0))
 
   # case $level in
-  # [0-20])
-    # icon=""
-    # ;;
-  # [21-40])
-    # icon=""
-    # ;;
-  # [41-60])
-    # icon=""
-    # ;;
-  # [61-80])
-    # icon=""
-    # ;;
-  # *)
-    # icon=""
-    # ;;
-  # esac
+  if ((0<=level && level<=20))
+  then
+    icon=""
+  elif ((21<=level && level<=40))
+  then
+    icon=""
+  elif ((41<=level && level<=60))
+  then
+    icon=""
+  elif ((61<=level && level<=80))
+  then
+    icon=""
+  else
+    icon=""
+  fi
+  echo -n "${charging} ${icon} ${level}%"
 
   # 
 
-  if [[ $(acpi) =~ "charged" ]]; then
-    # if on ac
-    charging=$(acpi | cut -d ' ' -f 4-6 | sed 's/until/до повної/')
-    # echo -n " ${level:0:2} ${charging}"
-    echo -n " ${charging}"
-  else
-    # if on battery
-    battery=$(acpi | cut -d ' ' -f 4-6 | sed 's/remaining/лишилося/')
-    echo -n " ${battery}"
-  fi
+  # this for linux
+  # if [[ $(acpi) =~ "charged" ]]; then
+    # # if on ac
+    # charging=$(acpi | cut -d ' ' -f 4-6 | sed 's/until/до повної/')
+    # # echo -n " ${level:0:2} ${charging}"
+    # echo -n " ${charging}"
+  # else
+    # # if on battery
+    # battery=$(acpi | cut -d ' ' -f 4-6 | sed 's/remaining/лишилося/')
+    # echo -n " ${battery}"
+  # fi
 }
