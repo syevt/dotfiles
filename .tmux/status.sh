e426a4aeb7555f1731157991fd04adae09e95741
Serhiy Yevtushenko <syevtush@cisco.com>
2019-10-06 09:26:45 +0300

    Add tmux status weather

--- .tmux/status.sh
+++ .tmux/status.sh
@@ -7,12 +7,18 @@ displayStatus() {
     displaySegment "session" "${TMUX_LIGHT_SEGMENT_FG}" "${TMUX_LIGHT_SEGMENT_BG}"
     echo "#[fg=${TMUX_LIGHT_SEGMENT_BG},bg=${TMUX_WINDOW_BG}]"
   else
+    # dark segment
+    # echo -n "#[fg=${TMUX_DARK_SEGMENT_BG},bg=${TMUX_DARK_SEGMENT_FG}]"
+    # light segment
+    echo -n "#[fg=${TMUX_LIGHT_SEGMENT_BG},bg=${TMUX_WINDOW_BG}]"
+    displaySegment "weather" "${TMUX_LIGHT_SEGMENT_FG}" "${TMUX_LIGHT_SEGMENT_BG}"
+  	echo -n "#[fg=${TMUX_WINDOW_BG},bg=${TMUX_DARK_SEGMENT_FG}]"
     displaySegment "battery" "${TMUX_DARK_SEGMENT_FG}" "${TMUX_WINDOW_BG}"
     echo -n "#[fg=${TMUX_LIGHT_SEGMENT_BG},bg=${TMUX_WINDOW_BG}]"
     displaySegment "continuum" "${TMUX_LIGHT_SEGMENT_FG}" "${TMUX_LIGHT_SEGMENT_BG}"
-  	echo -n "#[fg=${TMUX_DARK_SEGMENT_BG},bg=${TMUX_DARK_SEGMENT_FG}]"
-    displaySegment "time" "${TMUX_DARK_SEGMENT_FG}" "${TMUX_DARK_SEGMENT_BG}"
-  	echo -n "#[fg=${TMUX_LIGHT_SEGMENT_BG},bg=${TMUX_DARK_SEGMENT_BG}]"
+  	echo -n "#[fg=${TMUX_WINDOW_BG},bg=${TMUX_DARK_SEGMENT_FG}]"
+    displaySegment "time" "${TMUX_DARK_SEGMENT_FG}" "${TMUX_WINDOW_BG}"
+  	echo -n "#[fg=${TMUX_LIGHT_SEGMENT_BG},bg=${TMUX_WINDOW_BG}]"
     # displaySegment "hostname" "${TMUX_LIGHT_SEGMENT_FG}" "${TMUX_LIGHT_SEGMENT_BG}"
     displaySegment "date" "${TMUX_LIGHT_SEGMENT_FG}" "${TMUX_LIGHT_SEGMENT_BG}"
   fi
