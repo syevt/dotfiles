5a7694274c154464f18a7ec2d9e1ed3528a18527
Serhiy Yevtushenko <serhiy.yevtushenko@rotary.org>
2021-07-24 09:28:07 +0300

    Fix tmux color scheme

--- .tmux/gruvbox-dark.sh
+++ .tmux/gruvbox-dark.sh
@@ -8,7 +8,7 @@ export VIM_ALE_ERROR_BG="#3c3836"
 export VIM_ALE_WARNING_FG="#fe8019"
 export VIM_ALE_WARNING_BG="#3c3836"
 export VIM_STATUS_BAR_FG="#504945"
-export VIM_STATUS_BAR_BG="#a89984"
+export VIM_STATUS_BAR_BG="#af8787"
 
 # Tmux
 
@@ -16,25 +16,25 @@ export VIM_STATUS_BAR_BG="#a89984"
 export TMUX_STATUS_BG="#3c3836"
 
 # Borders
-export TMUX_PANE_ACTIVE_BORDER_FG="#fabd2f"
+export TMUX_PANE_ACTIVE_BORDER_FG="#ffaf00"
 export TMUX_PANE_BORDER_FG="#3c3836"
 
 # Messages
-export TMUX_MESSAGE_COMMAND_FG="#a89984"
+export TMUX_MESSAGE_COMMAND_FG="#af8787"
 export TMUX_MESSAGE_COMMAND_BG="#3c3836"
-export TMUX_MESSAGE_FG="#a89984"
+export TMUX_MESSAGE_FG="#af8787"
 export TMUX_MESSAGE_BG="#3c3836"
 
 # Status bar segments (session and hostname)
 export TMUX_LIGHT_SEGMENT_FG="#282828"
-export TMUX_LIGHT_SEGMENT_BG="#a89984"
+export TMUX_LIGHT_SEGMENT_BG="#af8787"
 
 # Dynamic segments
-export TMUX_DARK_SEGMENT_FG="#a89984"
+export TMUX_DARK_SEGMENT_FG="#af8787"
 export TMUX_DARK_SEGMENT_BG="#504945"
 
 # Windows
 export TMUX_CURRENT_WINDOW_FG="#282828"
-export TMUX_CURRENT_WINDOW_BG="#fabd2f"
-export TMUX_WINDOW_FG="#a89984"
+export TMUX_CURRENT_WINDOW_BG="#ffaf00"
+export TMUX_WINDOW_FG="#af8787"
 export TMUX_WINDOW_BG="#3c3836"
