394e8eb55f621e0282b89062a73abd6976124316
Serhiy Yevtushenko <syevtushenko@rotary.org>
2025-05-27 12:54:24 +0300

    Fixing making screenshots

--- .config/waybar/scripts/safe-screenshot.sh
+++ .config/waybar/scripts/safe-screenshot.sh
@@ -1,4 +1,26 @@
 #!/bin/bash
-pgrep slurp && exit 0  # If slurp is already running, don't launch again
+# File to save the screenshot
+# file="$HOME/Pictures/screenshot_$(date +%Y%m%d_%H%M%S).png"
 
-niri msg action screenshot
+# Use slurp to select an area and grim to capture it
+# area=$(slurp)
+# [ -z "$area" ] && exit 1  # cancel if user didn't select
+
+# grim -g "$area" "$file"
+
+# Optionally, copy to clipboard
+# wl-copy < "$file"
+
+# Optionally, open in an image viewer
+# swayimg "$file"
+
+# Select area with slurp
+area=$(slurp)
+[ -z "$area" ] && exit 1  # User hit escape or cancelled
+
+# Pipe grim output directly into wl-copy
+grim -g "$area" - | wl-copy --type image/png
+
+# Optional notification
+notify-send "📸 Screenshot" "Copied to clipboard"
+
