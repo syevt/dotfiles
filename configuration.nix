64be992ea426e715ed117f1a4d24c2ed10ff1c70
Serhiy Yevtushenko <syevtushenko@rotary.org>
2025-10-17 23:18:55 +0300

    Tidy up Nix config

--- configuration.nix
+++ configuration.nix
@@ -28,129 +28,141 @@ in {
     lib,
     ...
   }: {
-    home.stateVersion = "25.05"; # match your NixOS version
+    home = {
+      stateVersion = "25.05"; # match your NixOS version
 
-    # Clone dotfiles repo on first activation
-    home.activation.cloneDotfiles = lib.hm.dag.entryAfter ["writeBoundary"] ''
-      if [ ! -d "${dotfilesDir}" ]; then
-        ${pkgs.git}/bin/git clone https://github.com/${userName}/dotfiles.git "${dotfilesDir}"
-      fi
-    '';
-
-    programs.fzf = {
-      enable = true;
-      enableZshIntegration = true;
-      defaultCommand = "rg --files";
-      defaultOptions = ["--layout reverse"];
-    };
-
-    programs.direnv = {
-      enable = true;
-      enableZshIntegration = true;
-      nix-direnv.enable = true;
-    };
-
-    programs.tmux = {
-      enable = true;
-
-      baseIndex = 1;
-
-      plugins = with pkgs.tmuxPlugins; [
-        continuum
-        resurrect
-        vim-tmux-navigator
-      ];
-
-      extraConfig = ''
-        # Environment variable pointing to continuum plugin
-        set-environment -g TMUX_CONTINUUM_PATH ${pkgs.tmuxPlugins.continuum}/share/tmux-plugins/continuum
-        set-environment -g TMUX_RESURRECT_DIR "$HOME/.tmux/resurrect"
-
-        set -g @resurrect-strategy-vim 'session'
-        set -g @continuum-save-interval '5'
-        set -g @continuum-restore 'on'
-
-        source-file ${dotfilesDir}/.tmux.conf
-        run-shell ${pkgs.tmuxPlugins.continuum}/share/tmux-plugins/continuum/continuum.tmux
+      # Clone dotfiles repo on first activation
+      activation.cloneDotfiles = lib.hm.dag.entryAfter ["writeBoundary"] ''
+        if [ ! -d "${dotfilesDir}" ]; then
+          ${pkgs.git}/bin/git clone https://github.com/${userName}/dotfiles.git "${dotfilesDir}"
+        fi
       '';
+      file = let
+        link = path: config.lib.file.mkOutOfStoreSymlink "${dotfilesDir}/${path}";
+        files = [
+          ".alacritty.toml"
+          ".bash_aliases"
+          ".config/nvim"
+          ".config/fuzzel/fuzzel.ini"
+          ".gitconfig"
+          ".config/hypr"
+          ".config/niri"
+          ".tmux/segments/"
+          ".tmux/gruvbox-dark.sh"
+          ".tmux/segment.sh"
+          ".tmux/status.sh"
+          ".tmux/window.sh"
+          ".tridactylrc"
+          ".config/waybar"
+          ".config/wlogout"
+        ];
+      in
+        builtins.listToAttrs (map (path: {
+            name = path;
+            value.source = link path;
+          })
+          files);
     };
 
-    programs.zsh = {
-      enable = true;
-      enableCompletion = true;
-      history = {
-        saveNoDups = true;
-        # these two are needed to access history inside `nix develop`
-        append = true;
-        share = true;
-      };
-      syntaxHighlighting.enable = true;
-      autosuggestion = {
+    programs = {
+      fzf = {
         enable = true;
-        strategy = ["match_prev_cmd"];
+        enableZshIntegration = true;
+        defaultCommand = "rg --files";
+        defaultOptions = ["--layout reverse"];
       };
 
-      oh-my-zsh = {
+      direnv = {
         enable = true;
-        plugins = ["git" "vi-mode" "yarn"];
-        custom = "${dotfilesDir}/zsh_custom";
+        enableZshIntegration = true;
+        nix-direnv.enable = true;
       };
 
-      initContent = ''
-        source ${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k/powerlevel10k.zsh-theme
-        source ${dotfilesDir}/.zshrc
-      '';
+      tmux = {
+        enable = true;
+        baseIndex = 1;
+        plugins = with pkgs.tmuxPlugins; [
+          continuum
+          resurrect
+          vim-tmux-navigator
+        ];
+
+        extraConfig = ''
+          # Environment variable pointing to continuum plugin
+          set-environment -g TMUX_CONTINUUM_PATH ${pkgs.tmuxPlugins.continuum}/share/tmux-plugins/continuum
+          set-environment -g TMUX_RESURRECT_DIR "$HOME/.tmux/resurrect"
+
+          set -g @resurrect-strategy-vim 'session'
+          set -g @continuum-save-interval '5'
+          set -g @continuum-restore 'on'
+
+          source-file ${dotfilesDir}/.tmux.conf
+          run-shell ${pkgs.tmuxPlugins.continuum}/share/tmux-plugins/continuum/continuum.tmux
+        '';
+      };
+
+      zsh = {
+        enable = true;
+        enableCompletion = true;
+        history = {
+          saveNoDups = true;
+          # these two are needed to access history inside `nix develop`
+          append = true;
+          share = true;
+        };
+        syntaxHighlighting.enable = true;
+        autosuggestion = {
+          enable = true;
+          strategy = ["match_prev_cmd"];
+        };
+
+        oh-my-zsh = {
+          enable = true;
+          plugins = ["git" "vi-mode" "yarn"];
+          custom = "${dotfilesDir}/zsh_custom";
+        };
+
+        initContent = ''
+          source ${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k/powerlevel10k.zsh-theme
+          source ${dotfilesDir}/.zshrc
+        '';
+      };
     };
-
-    home.file.".alacritty.toml".source =
-      config.lib.file.mkOutOfStoreSymlink "${dotfilesDir}/.alacritty.toml";
-    home.file.".bash_aliases".source = config.lib.file.mkOutOfStoreSymlink "${dotfilesDir}/.bash_aliases";
-    home.file.".config/nvim".source =
-      config.lib.file.mkOutOfStoreSymlink "${dotfilesDir}/.config/nvim";
-    home.file.".config/fuzzel/fuzzel.ini".source =
-      config.lib.file.mkOutOfStoreSymlink "${dotfilesDir}/.config/fuzzel/fuzzel.ini";
-    home.file.".gitconfig".source = config.lib.file.mkOutOfStoreSymlink "${dotfilesDir}/.gitconfig";
-    home.file.".config/hypr".source = config.lib.file.mkOutOfStoreSymlink "${dotfilesDir}/.config/hypr";
-    home.file.".config/niri".source = config.lib.file.mkOutOfStoreSymlink "${dotfilesDir}/.config/niri";
-
-    home.file.".tmux/segments/".source = config.lib.file.mkOutOfStoreSymlink "${dotfilesDir}/.tmux/segments/";
-    home.file.".tmux/gruvbox-dark.sh".source =
-      config.lib.file.mkOutOfStoreSymlink "${dotfilesDir}/.tmux/gruvbox-dark.sh";
-    home.file.".tmux/segment.sh".source = config.lib.file.mkOutOfStoreSymlink "${dotfilesDir}/.tmux/segment.sh";
-    home.file.".tmux/status.sh".source = config.lib.file.mkOutOfStoreSymlink "${dotfilesDir}/.tmux/status.sh";
-    home.file.".tmux/window.sh".source = config.lib.file.mkOutOfStoreSymlink "${dotfilesDir}/.tmux/window.sh";
-
-    home.file.".tridactylrc".source = config.lib.file.mkOutOfStoreSymlink "${dotfilesDir}/.tridactylrc";
-    home.file.".config/waybar".source = config.lib.file.mkOutOfStoreSymlink "${dotfilesDir}/.config/waybar";
-    home.file.".config/wlogout".source = config.lib.file.mkOutOfStoreSymlink "${dotfilesDir}/.config/wlogout";
   };
 
   # Bootloader.
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
+  boot = {
+    loader.systemd-boot.enable = true;
+    loader.efi.canTouchEfiVariables = true;
+    # Use latest kernel.
+    kernelPackages = pkgs.linuxPackages_latest;
+    kernelModules = pkgs.lib.mkAfter ["ecryptfs"];
+  };
 
-  # Use latest kernel.
-  boot.kernelPackages = pkgs.linuxPackages_latest;
+  networking = {
+    hostName = "nixos"; # Define your hostname.
+    # Configure network proxy if necessary
+    # proxy.default = "http://user:password@proxy:port/";
+    # proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+    # Enable networking
+    networkmanager.enable = true;
+    networkmanager.unmanaged = [];
+    wireless.enable = false; # Enables wireless support via wpa_supplicant.
+    nameservers = ["1.1.1.1" "8.8.8.8"];
+    # Open ports in the firewall.
+    # firewall.allowedTCPPorts = [ ... ];
+    # firewall.allowedUDPPorts = [ ... ];
+    # Or disable the firewall altogether.
+    # firewall.enable = false;
+  };
 
-  boot.kernelModules = pkgs.lib.mkAfter ["ecryptfs"];
-
-  networking.hostName = "nixos"; # Define your hostname.
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Enable networking
-  networking.networkmanager.enable = true;
-  networking.networkmanager.unmanaged = [];
-  networking.wireless.enable = false; # Enables wireless support via wpa_supplicant.
-
-  networking.nameservers = ["1.1.1.1" "8.8.8.8"];
-
-  # Bluetooth
-  hardware.bluetooth = {
-    enable = true;
-    powerOnBoot = true;
+  hardware = {
+    # Bluetooth
+    bluetooth = {
+      enable = true;
+      powerOnBoot = true;
+    };
+    graphics.enable = true;
   };
 
   # Set your time zone.
@@ -171,59 +183,56 @@ in {
     LC_TIME = "uk_UA.UTF-8";
   };
 
-  # Enable the X11 windowing system.
-  # You can disable this if you're only using the Wayland session.
-  services.xserver.enable = true;
-
-  # services.systembus-notify.enable = true;
-
-  hardware.graphics.enable = true;
-
-  services.xserver.videoDrivers = ["amdgpu"];
-  # hardware.opengl = {
-  #   enable = true;
-  #   extraPackages = [ pkgs.vaapiVdpau pkgs.libvdpau-va-gl ];
-  # };
-
-  # Enable the KDE Plasma Desktop Environment.
-  services.displayManager.sddm.enable = true;
-  services.desktopManager.plasma6.enable = true;
-
-  # Configure keymap in X11
-  services.xserver.xkb = {
-    layout = "us";
-    variant = "dvorak";
-  };
-
+  security.rtkit.enable = true;
   # Configure console keymap
   console.keyMap = "dvorak";
 
-  # Enable CUPS to print documents.
-  services.printing.enable = true;
+  # Enable the X11 windowing system.
+  # You can disable this if you're only using the Wayland session.
+  services = {
+    # services.systembus-notify.enable = true;
+    xserver = {
+      enable = true;
+      videoDrivers = ["amdgpu"];
+      # Configure keymap in X11
+      xkb = {
+        layout = "us";
+        variant = "dvorak";
+      };
+      # Enable the OpenSSH daemon.
+      # services.openssh.enable = true;
+    };
 
-  # Enable sound with pipewire.
-  services.pulseaudio.enable = false;
-  security.rtkit.enable = true;
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
+    # Enable the KDE Plasma Desktop Environment.
+    displayManager.sddm.enable = true;
+    desktopManager.plasma6.enable = true;
 
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
+    # Enable CUPS to print documents.
+    printing.enable = true;
+
+    # Enable sound with pipewire.
+    pulseaudio.enable = false;
+    pipewire = {
+      enable = true;
+      alsa.enable = true;
+      alsa.support32Bit = true;
+      pulse.enable = true;
+      # If you want to use JACK applications, uncomment this
+      #jack.enable = true;
+
+      # use the example session manager (no others are packaged yet so this is enabled by default,
+      # no need to redefine it in your config for now)
+      #media-session.enable = true;
+    };
+
+    tor = {
+      enable = true;
+      client.enable = true;
+    };
+
+    # Enable touchpad support (enabled default in most desktopManager).
+    # libinput.enable = true;
   };
-
-  services.tor = {
-    enable = true;
-    client.enable = true;
-  };
-
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.${userName} = {
@@ -245,19 +254,25 @@ in {
     weather-icons
   ];
 
-  # Install firefox.
-  programs.firefox.enable = true;
-
-  # zsh
-  programs.zsh.enable = true;
-  # programs.zsh.promptInit = "source ${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k/powerlevel10k.zsh-theme";
-  # users.defaultUserShell = pkgs.zsh;
-
-  # that's the hack to make things like `nodenv` work
-  programs.nix-ld.enable = true;
-
-  programs.niri.enable = true;
-  programs.nm-applet.enable = true;
+  programs = {
+    # Install firefox.
+    firefox.enable = true;
+    # zsh
+    zsh.enable = true;
+    # zsh.promptInit = "source ${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k/powerlevel10k.zsh-theme";
+    # users.defaultUserShell = pkgs.zsh;
+    # that's the hack to make things like `nodenv` work
+    nix-ld.enable = true;
+    niri.enable = true;
+    nm-applet.enable = true;
+    # Some programs need SUID wrappers, can be configured further or are
+    # started in user sessions.
+    # mtr.enable = true;
+    # gnupg.agent = {
+    #   enable = true;
+    #   enableSSHSupport = true;
+    # };
+  };
 
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
@@ -271,7 +286,6 @@ in {
   # $ nix search wget
   environment.systemPackages = with pkgs; [
     #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-    #  wget
     alacritty
     alejandra
     audacious
@@ -337,25 +351,6 @@ in {
     zsh
     zsh-powerlevel10k
   ];
-
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
