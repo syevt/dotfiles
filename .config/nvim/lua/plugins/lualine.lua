03155c94ca74426675ac8708640a11b219ef68c7
Serhiy Yevtushenko <syevtushenko@rotary.org>
2025-10-18 10:24:24 +0300

    Remove component separators in lualine

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -1,11 +1,14 @@
-  return {
-    'nvim-lualine/lualine.nvim',
-    dependencies = { 'nvim-tree/nvim-web-devicons' },
-    config = function()
-      require("lualine").setup({
-        sections = {
-          lualine_x = {'encoding', 'fileformat', 'filetype', 'lsp_status'},
-        },
-      })
-    end,
-  }
+return {
+  "nvim-lualine/lualine.nvim",
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  config = function()
+    require("lualine").setup({
+      options = {
+        component_separators = { left = "", right = "" },
+      },
+      sections = {
+        lualine_x = { "encoding", "fileformat", "filetype", "lsp_status" },
+      },
+    })
+  end,
+}
