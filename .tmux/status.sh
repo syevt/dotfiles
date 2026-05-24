5eb54b368b506ac4f21b8f8522fd4aa9b37195f0
Serhiy Yevtushenko <syevtushenko@rotary.org>
2026-05-24 16:22:17 +0300

    Non-relevant changes

--- .tmux/status.sh
+++ .tmux/status.sh
@@ -10,17 +10,19 @@ displayStatus() {
     # dark segment
     # echo -n "#[fg=${TMUX_DARK_SEGMENT_BG},bg=${TMUX_DARK_SEGMENT_FG}]"
     # light segment
-    echo -n "#[fg=${TMUX_LIGHT_SEGMENT_BG},bg=${TMUX_WINDOW_BG}]"
-    displaySegment "weather" "${TMUX_LIGHT_SEGMENT_FG}" "${TMUX_LIGHT_SEGMENT_BG}"
-  	echo -n "#[fg=${TMUX_WINDOW_BG},bg=${TMUX_DARK_SEGMENT_FG}]"
-    displaySegment "battery" "${TMUX_DARK_SEGMENT_FG}" "${TMUX_WINDOW_BG}"
+   #  echo -n "#[fg=${TMUX_LIGHT_SEGMENT_BG},bg=${TMUX_WINDOW_BG}]"
+   #  displaySegment "weather" "${TMUX_LIGHT_SEGMENT_FG}" "${TMUX_LIGHT_SEGMENT_BG}"
+  	# echo -n "#[fg=${TMUX_WINDOW_BG},bg=${TMUX_DARK_SEGMENT_FG}]"
+   #  displaySegment "battery" "${TMUX_DARK_SEGMENT_FG}" "${TMUX_WINDOW_BG}"
+   #  echo -n "#[fg=${TMUX_LIGHT_SEGMENT_BG},bg=${TMUX_WINDOW_BG}]"
+   #  displaySegment "continuum" "${TMUX_LIGHT_SEGMENT_FG}" "${TMUX_LIGHT_SEGMENT_BG}"
+  	# echo -n "#[fg=${TMUX_WINDOW_BG},bg=${TMUX_DARK_SEGMENT_FG}]"
+   #  displaySegment "time" "${TMUX_DARK_SEGMENT_FG}" "${TMUX_WINDOW_BG}"
+  	# echo -n "#[fg=${TMUX_LIGHT_SEGMENT_BG},bg=${TMUX_WINDOW_BG}]"
+    # displaySegment "hostname" "${TMUX_LIGHT_SEGMENT_FG}" "${TMUX_LIGHT_SEGMENT_BG}"
+    # displaySegment "date" "${TMUX_LIGHT_SEGMENT_FG}" "${TMUX_LIGHT_SEGMENT_BG}"
     echo -n "#[fg=${TMUX_LIGHT_SEGMENT_BG},bg=${TMUX_WINDOW_BG}]"
     displaySegment "continuum" "${TMUX_LIGHT_SEGMENT_FG}" "${TMUX_LIGHT_SEGMENT_BG}"
-  	echo -n "#[fg=${TMUX_WINDOW_BG},bg=${TMUX_DARK_SEGMENT_FG}]"
-    displaySegment "time" "${TMUX_DARK_SEGMENT_FG}" "${TMUX_WINDOW_BG}"
-  	echo -n "#[fg=${TMUX_LIGHT_SEGMENT_BG},bg=${TMUX_WINDOW_BG}]"
-    # displaySegment "hostname" "${TMUX_LIGHT_SEGMENT_FG}" "${TMUX_LIGHT_SEGMENT_BG}"
-    displaySegment "date" "${TMUX_LIGHT_SEGMENT_FG}" "${TMUX_LIGHT_SEGMENT_BG}"
   fi
 
   exit 0
